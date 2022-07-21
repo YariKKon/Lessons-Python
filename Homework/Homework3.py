f8469b226a336340f243c753015fcb0282de827f
YariK <xudeku92@gmail.com>
2022-07-21 19:42:26 +0300

    add Home Worke 3

---
+++ Homework/Homework3.py
@@ -0,0 +1,80 @@
+# 1 - Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# *Пример:*
+
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+def sum_odd_index(lst):
+    s = 0
+    for i in range(len(lst)):
+        if i % 2 != 0:
+            s += lst[i]
+    print(f"The sum is equal to: {s}")
+
+
+lst = list(map(int, input("Enter the numbers separated by a space:\n").split()))
+sum_odd_index(lst)
+
+# 2 - Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# *Пример:*
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+
+
+lst = list(map(int, input("Enter the numbers separated by a space:\n").split()))
+mult_lst(lst)
+
+# 3 - Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# *Пример:*
+
+# - [1.1, 1.2, 3.1, 5.567, 10.003] => 0.564 или 564
+
+lst = list(map(float, input("Введите числа через пробел:\n").split()))
+new_lst = [round(i%1,2) for i in lst if i%1 != 0]
+print(max(new_lst) - min(new_lst))
+
+# 4 - Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# *Пример:*
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+s = ""
+n = int(input("Enter a number to convert decimal to binary:\n"))
+while n != 0:
+    s = str(n%2) + s
+    n //=2
+print(s)
+
+# 5 - Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# *Пример:*
+
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи](https://clck.ru/sH87m)
+
+n = int(input('Введите число: '))
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+fibo_nums = get_fibonacci(n)
+print(get_fibonacci(n))
